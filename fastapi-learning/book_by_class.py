26d1e710b6b50b092d44b421e67fdc80ebe72e58
Kogul <kogul2701@gmail.com>
2024-05-12 09:13:44 +0200

    Removed the duplicated entry

--- fastapi-learning/book_by_class.py
+++ fastapi-learning/book_by_class.py
@@ -15,13 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class Book:
     id: int
